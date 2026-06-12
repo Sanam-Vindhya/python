07b5a8c359154eb89092fc2b118d045333c0ee5f
Sanam-Vindhya <sanamvindhya@gmail.com>
2026-06-12 15:01:30 +0530

    Add files via upload

---
+++ logicAND.py
@@ -0,0 +1,6 @@
+marks=int(input("Enter marks:"))
+attendence=int(input("Enter attendence:"))
+if marks>35 and attendence>=75:
+    print("Student is eligible for the exam.")
+else:
+    print("Student is not eligible for the exam.")
