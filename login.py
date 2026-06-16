995cd5093dfd4037da5add2fb7db21a3f2e9c810
Sanam-Vindhya <sanamvindhya@gmail.com>
2026-06-16 13:34:28 +0530

    Add files via upload

---
+++ login.py
@@ -0,0 +1,8 @@
+name="vindhya"
+username=input("Enter username:")
+id=402329
+password=input("Enter password:")
+if username==name and password==str(id):
+    print("Login successful.")
+else:
+    print("Invalid username or password.")
